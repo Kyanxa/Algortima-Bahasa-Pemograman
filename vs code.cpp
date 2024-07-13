7fe226b49efaf864ccee3a145e26b6348fa3a3ca
Kyanxa <catherine29805@gmail.com>
2024-07-13 14:22:52 +0700

    Add files via upload

---
+++ vs code.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+class MyClass {
+  public:
+    int myNum;
+    string myString;
+};
+
+int main() {
+  MyClass myObj;
+
+  myObj.myNum = 15; 
+  myObj.myString = "Some text";
+
+  cout << myObj.myNum << "\n";
+  cout << myObj.myString;
+  return 0;
+}
